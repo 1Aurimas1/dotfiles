22eb8d81c82223d55ea529346d3fa64cb50de911
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2022-11-13 16:59:46 +0000

    Initial commit

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,26 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
+  --Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'sbdchd/neoformat'
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use {'catppuccin/nvim', as = 'catppuccin'}
+  use { "ellisonleao/gruvbox.nvim", as = 'gruvbox'}
+    --use("hrsh7th/cmp-buffer")
+  
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
