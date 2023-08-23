622b0ef8779abfa6429bacece5c2ef6b6bc1dfd1
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2023-08-24 02:56:58 +0300

    Some keymap readjustments

---
+++ nvim/after/plugin/harpoon.lua
@@ -0,0 +1,14 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+
+--vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+--vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+--vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+--vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+--nmap { "<M-h><M-l>", require("harpoon.ui").toggle_quick_menu }
+for i = 1, 4 do
+    vim.keymap.set("n", string.format("<leader>%s", i), function() ui.nav_file(i) end)
+end
--- nvim/init.lua
+++ nvim/init.lua
@@ -230,20 +230,20 @@ vim.g.clipboard = {
     cache_enabled = false
 }
 
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
---vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
---vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
---vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
---vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
-
-for i = 1, 4 do
-    vim.keymap.set("n", string.format("<leader>%s", i), function() ui.nav_file(i) end)
-end
+--local mark = require("harpoon.mark")
+--local ui = require("harpoon.ui")
+--
+--vim.keymap.set("n", "<leader>a", mark.add_file)
+--vim.keymap.set("n", "<leader><C-e>", ui.toggle_quick_menu)
+--
+----vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+----vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+----vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+----vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+----nmap { "<M-h><M-l>", require("harpoon.ui").toggle_quick_menu }
+--for i = 1, 4 do
+--    vim.keymap.set("n", string.format("<leader>%s", i), function() ui.nav_file(i) end)
+--end
 
 -- prompt for a refactor to apply when the remap is triggered
 vim.api.nvim_set_keymap(
