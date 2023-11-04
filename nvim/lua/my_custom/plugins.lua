31e9bc60e39c0fb4f1a67daa566ba5957933a920
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2023-11-04 15:35:21 +0200

    Migrate packer to lazy

--- nvim/lua/my_custom/plugins.lua
+++ nvim/lua/my_custom/plugins.lua
@@ -1,66 +1,60 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--depth=1",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-    --Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use 'sbdchd/neoformat'
-    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+require("lazy").setup({
+	"sbdchd/neoformat",
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-    use {'catppuccin/nvim', as = 'catppuccin'}
-    use { "ellisonleao/gruvbox.nvim", as = 'gruvbox'}
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-    use "rebelot/kanagawa.nvim"
-    use 'folke/tokyonight.nvim'
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "ellisonleao/gruvbox.nvim", name = "gruvbox" },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	"rebelot/kanagawa.nvim",
+	"folke/tokyonight.nvim",
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use("theprimeagen/refactoring.nvim")
-    use("theprimeagen/harpoon")
-    use("mbbill/undotree")
-    use("nvim-treesitter/nvim-treesitter-context");
-    --use("hrsh7th/cmp-buffer")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	"theprimeagen/refactoring.nvim",
+	"theprimeagen/harpoon",
+	"mbbill/undotree",
+	"nvim-treesitter/nvim-treesitter-context",
 
-    --use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-    --use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    --use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    --use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    --use 'L3MON4D3/LuaSnip' -- Snippets plugin
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
-
-    use('ThePrimeagen/vim-be-good');
-    use('tpope/vim-fugitive');
-
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+	"ThePrimeagen/vim-be-good",
+	"tpope/vim-fugitive",
+})
