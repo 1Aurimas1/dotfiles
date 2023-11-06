28b75cb1ecd93140b6b7e7cf2de93e04f9403946
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2023-11-07 01:42:06 +0200

    Return to lspconfig

--- nvim/lua/my_custom/plugins.lua
+++ nvim/lua/my_custom/plugins.lua
@@ -32,28 +32,28 @@ require("lazy").setup({
 	"mbbill/undotree",
 	"nvim-treesitter/nvim-treesitter-context",
 
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v1.x",
-		dependencies = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+	--{
+	--	"VonHeikemen/lsp-zero.nvim",
+	--	branch = "v1.x",
+	--	dependencies = {
+	-- LSP Support
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
+	-- Autocompletion
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-nvim-lua" },
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	},
+	-- Snippets
+	{ "L3MON4D3/LuaSnip" },
+	{ "rafamadriz/friendly-snippets" },
+	--	},
+	--},
 
 	"ThePrimeagen/vim-be-good",
 	"tpope/vim-fugitive",
